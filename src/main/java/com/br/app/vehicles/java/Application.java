70441e651fa96555bfc8f6ef2db6af2eb342303a
leonardo dantas <leonardo1317@yahoo.com.br>
2025-04-28 18:09:38 -0300

    feat: Criando docker compose e configurando mongo

--- src/main/java/com/br/app/vehicles/java/Application.java
+++ src/main/java/com/br/app/vehicles/java/Application.java
@@ -1,13 +1,13 @@
-package com.br.app.vehicles.app_vehicles;
+package com.br.app.vehicles.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppVehiclesJavaApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppVehiclesJavaApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
