6a087916249cf96e5db49671074378a0e85ea393
leonardo dantas <leonardo1317@yahoo.com.br>
2025-04-28 18:03:00 -0300

    feat: Iniciando projeto

---
+++ src/main/java/com/br/app/vehicles/app_vehicles/AppVehiclesJavaApplication.java
@@ -0,0 +1,13 @@
+package com.br.app.vehicles.app_vehicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppVehiclesJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppVehiclesJavaApplication.class, args);
+	}
+
+}
