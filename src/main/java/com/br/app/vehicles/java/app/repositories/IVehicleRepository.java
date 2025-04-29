3cd03d7761aa06ffa8bec322fbe2cac89c3ae9f2
leonardo dantas <leonardo1317@yahoo.com.br>
2025-04-28 22:45:48 -0300

    feat: Novos metodos para acesso no banco

--- src/main/java/com/br/app/vehicles/java/app/repositories/IVehicleRepository.java
+++ src/main/java/com/br/app/vehicles/java/app/repositories/IVehicleRepository.java
@@ -2,6 +2,12 @@ package com.br.app.vehicles.java.app.repositories;
 
 import com.br.app.vehicles.java.domain.Vehicle;
 
+import java.util.Optional;
+
 public interface IVehicleRepository {
     Vehicle save(final Vehicle vehicle);
+
+    Optional<Vehicle> findById(final String id);
+
+    void delete(final String id);
 }
