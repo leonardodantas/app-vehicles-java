bb718c98f47c0e17c53e91c2be3044243f033962
leonardo dantas <leonardo1317@yahoo.com.br>
2025-04-28 22:47:49 -0300

    feat: Alterando nome do request

--- src/main/java/com/br/app/vehicles/java/infra/controller/jsons/requests/VehicleRequest.java
+++ src/main/java/com/br/app/vehicles/java/infra/controller/jsons/requests/VehicleRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record VehicleCreateRequest(
+public record VehicleRequest(
         @MarkValid
         String mark,
         @Min(value = 1886, message = "{year.min}")
