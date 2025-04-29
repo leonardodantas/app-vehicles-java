cddb1db0960d323f216abb9f0bff6b7c0b008c1e
leonardo dantas <leonardo1317@yahoo.com.br>
2025-04-28 22:48:07 -0300

    feat: Novos metodos no mapper

--- src/main/java/com/br/app/vehicles/java/infra/controller/mappers/VehicleMapper.java
+++ src/main/java/com/br/app/vehicles/java/infra/controller/mappers/VehicleMapper.java
@@ -1,14 +1,19 @@
 package com.br.app.vehicles.java.infra.controller.mappers;
 
 import com.br.app.vehicles.java.domain.Vehicle;
-import com.br.app.vehicles.java.infra.controller.jsons.requests.VehicleCreateRequest;
+import com.br.app.vehicles.java.infra.controller.jsons.requests.VehicleRequest;
+import com.br.app.vehicles.java.infra.controller.jsons.requests.VehicleUpdateRequest;
 import com.br.app.vehicles.java.infra.controller.jsons.responses.VehicleResponse;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface VehicleMapper {
 
-    Vehicle toDomain(final VehicleCreateRequest request);
+    Vehicle toDomain(final VehicleRequest request);
+
+    Vehicle toDomain(final String id, final VehicleRequest request);
+
+    Vehicle toDomain(final String id, final VehicleUpdateRequest request);
 
     VehicleResponse toResponse(final Vehicle vehicle);
 }
